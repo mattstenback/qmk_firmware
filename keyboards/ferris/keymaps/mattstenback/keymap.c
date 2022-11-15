b754bd57b6c44d96805f0cb34ba724d229b9fe9f
mattstenback <mstenback@gmail.com>
2022-11-15 16:06:37 -0330

    Added the swapper tab functionality back to the keymap. Tweaked the combos again.

--- keyboards/ferris/keymaps/mattstenback/keymap.c
+++ keyboards/ferris/keymaps/mattstenback/keymap.c
@@ -64,6 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ONEPW,   SC_SHOT, BR_BACK, BR_FORW, KC_MPLY, KC_HOME,  KC_PGDN, KC_PGUP, KC_END,   KC_ENT,
                                    _______, _______, _______, _______
     ),
+    
 
     [NUM] = LAYOUT(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
