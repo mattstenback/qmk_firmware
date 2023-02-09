5fb7c234f4319aeab10434ea349a5a8933b80cfe
Matt Stenback <mstenback@gmail.com>
2023-02-09 20:15:23 -0330

    Added ferris keymap for mini36. Updated the scrolling OLED to my own message.

---
+++ keyboards/controllerworks/mini36/keymaps/mattstenback/rules.mk
@@ -0,0 +1,9 @@
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = no
+
+# Enables combos to be defined cleanly in combos.def
+COMBO_ENABLE = yes
+VPATH += keyboards/gboards
+
+SRC += oneshot.c
+SRC += swapper.c
